dad7199db15902d0d2a3be20b070cf94f50834cc
Timothy-Manwell <113055850+Timothy-Manwell@users.noreply.github.com>
2022-10-06 14:25:00 -0400

    init

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// Copyright (c) 2020 Mr. Coxall All rights reserved
+//
+// Created by: Mr. Coxall
+// Created on: Sep 2020
+// This file contains the JS functions for index.html
+
+/**
+ * Find Area and Perimeter
+ */
+function calculateClicked() {
+  document.getElementById("area").innerHTML = "<p>The Area is: " + (27 * 52) + "</p>"
+  document.getElementById("perimeter").innerHTML = "<p>The Perimeter is: " + (2 * (27 + 52)) + "</p>"
+}

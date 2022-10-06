4dbca54a298f19ac0df1dc0519d76d5041a00afe
Timothy-Manwell <Timothy-Manwell@users.noreply.github.com>
2022-10-06 18:27:03 +0000

    Prettified Code!

--- js/script.js
+++ js/script.js
@@ -8,6 +8,8 @@
  * Find Area and Perimeter
  */
 function calculateClicked() {
-  document.getElementById("area").innerHTML = "<p>The Area is: " + (27 * 52) + "</p>"
-  document.getElementById("perimeter").innerHTML = "<p>The Perimeter is: " + (2 * (27 + 52)) + "</p>"
+  document.getElementById("area").innerHTML =
+    "<p>The Area is: " + 27 * 52 + "</p>"
+  document.getElementById("perimeter").innerHTML =
+    "<p>The Perimeter is: " + 2 * (27 + 52) + "</p>"
 }
